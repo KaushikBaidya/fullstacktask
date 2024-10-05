e22d5a63ba720856769c61c77499cb88d8fc3f04
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2024-10-06 00:44:57 +0600

    added navbar footer and hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Forms from '@/components/Forms';
 import GetTodos from '@/components/GetTodos';
+import Hero from '@/components/pageComponents/Hero';
 
 export default function Home() {
 	return (
-		<div className='min-h-screen relative bg-slate-200'>
+		<div className='min-h-screen relative'>
+			<div>
+				<Hero />
+			</div>
 			<div className='flex justify-around flex-col items-center h-1/2 '>
 				<h1 className=' text-4xl font-bold mt-12 mb-12'>
 					Todos Page (Kaushik)
