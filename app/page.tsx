6205f398815520eac12318b19b5133e1fd43cb4b
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2024-10-06 12:05:10 +0600

    updated without hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import GetTodos from '@/components/GetTodos';
-import { Featured } from '@/components/pageComponents/Featured';
-import Hero from '@/components/pageComponents/Hero';
+import { Hero } from '@/components/pageComponents/Hero';
 
 export default function Home() {
 	return (
 		<div className='min-h-screen relative'>
 			<div>
 				<Hero />
-				<Featured />
 			</div>
 		</div>
 	);
--- components/pageComponents/Hero.tsx
+++ components/pageComponents/Hero.tsx
@@ -1,41 +1,63 @@
-'use client';
-import React from 'react';
+import * as React from 'react';
+import { connectToMongoDB } from '@/lib/db';
+import Library from '@/models/libraryModel';
+
+import { Card, CardContent } from '@/components/ui/card';
+
 import Image from 'next/image';
 import { FiCompass } from 'react-icons/fi';
-import HeroImage from '@/assets/heroimg.png';
+import Link from 'next/link';
+import { Button } from '../ui/button';
 
-const Hero: React.FC = () => {
+async function fetchBooks() {
+	await connectToMongoDB();
+	const books = await Library.find();
+	return books;
+}
+
+export async function Hero() {
+	const response = await fetchBooks();
+	const books = response.slice(0, 8);
 	return (
-		<main className='pt-36 md:pt-28'>
-			<section className='container mx-auto flex flex-col md:flex-row items-center justify-center'>
-				<div className='text-center md:text-left md:w-1/2 px-2 md:pl-5 3xl:pl-0'>
-					<h1 className='text-4xl md:text-7xl text-gray-800 font-bold mb-4'>
-						Discover Your Next Favourite Book
-					</h1>
-					<p className='text-lg text-gray-500 mb-6'>
-						Explore a growing collection of books, manage your personal library,
-						and keep track of the books you have read or want to read.
-					</p>
-					<div className='flex justify-center md:justify-start'>
-						<button className='bg-black text-white px-4 py-2 rounded-md flex items-center hover:bg-slate-600'>
-							<FiCompass className='mr-2' />
-							Browse All Collection
-						</button>
-					</div>
-				</div>
+		<section className='container mx-auto my-8 pt-20'>
+			<h2 className='text-3xl font-bold mb-4 text-center'>Featured Books</h2>
 
-				<div className='md:w-1/2 mt-6 md:mt-0 flex justify-center'>
-					<Image
-						src={HeroImage}
-						width={600}
-						height={500}
-						className='hidden md:block w-full h-auto'
-						alt='Libaria'
-					/>
+			<div className='w-full max-w-6xl mx-auto'>
+				<div className='w-full flex flex-wrap items-center justify-center gap-5'>
+					{books.map((book) => (
+						<div key={book.id} className='flex'>
+							<Card className='w-56 h-full flex flex-col items-center shadow-lg rounded-lg overflow-hidden transition-transform transform hover:scale-105'>
+								<div className='flex-shrink-0 h-52'>
+									<Image
+										src={book.image}
+										alt={book.title}
+										width={150}
+										height={200}
+										className='w-full h-48 object-cover'
+									/>
+								</div>
+								<CardContent className='flex flex-col justify-between p-4 h-full'>
+									<h3 className='font-semibold text-center mb-2'>
+										{book.title}
+									</h3>
+									<p className='text-sm text-gray-600 text-center mb-4'>
+										{book.author}
+									</p>
+								</CardContent>
+							</Card>
+						</div>
+					))}
 				</div>
-			</section>
-		</main>
+			</div>
+
+			<div className='flex justify-center py-5 mx-auto'>
+				<Link href='allbooks'>
+					<button className='bg-black text-white px-4 py-2 rounded-md flex items-center hover:bg-slate-600'>
+						<FiCompass className='mr-2' />
+						Browse All Collection
+					</button>
+				</Link>
+			</div>
+		</section>
 	);
-};
-
-export default Hero;
+}
